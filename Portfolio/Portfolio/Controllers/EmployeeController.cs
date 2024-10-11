bc2e6f7c9005b3c8c170db391f236471276873e4
YasinElka <169077740+YasinElka@users.noreply.github.com>
2024-10-11 08:20:40 +0200

    Created project

---
+++ Portfolio/Portfolio/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            MongoClient dbClient = new MongoClient();
+
+            var database = dbClient.GetDatabase("portfolio");
+            var collection = database.GetCollection<Employee>("employee");
+
+            List<Employee> employee = collection.Find(e => true).ToList();
+
+            return View(employee);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            MongoClient dbClient = new MongoClient();
+
+            var database = dbClient.GetDatabase("portfolio");
+            var collection = database.GetCollection<Employee>("employee");
+            collection.InsertOne(employee);
+
+            return Redirect("/Employee");
+        }
+
+
+
+
+
+    }
+}
---
+++ Portfolio/Portfolio/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace Portfolio.Models
+{
+    public class Employee
+    {
+        public ObjectId Id { get; set; }
+        public String Name { get; set; }
+
+        public int Birthyear { get; set; }
+    }
+}
